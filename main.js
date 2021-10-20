114bd578e4c9a11e1493b81d802a2be997ac02fd
ruruisu <zaddozgl@gmail.com>
2021-10-19 19:43:35 -0500

    add function

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 class Product{
-    constructor(idCode,name){
-        this.name=name;
-        this.idCode=idCode;
+    constructor(id,name){
+        this._id = id;
+        this._name = name;
+        this._units = units;
+        this._cost = cost;
+        this._totalCost = this._units * this._cost;
         this.next=null;
     }
 }
 class Inventory{
     constructor(){
-    }  
+        this.start=null;
+    }
+    add(){
+
+    }
 }
 class App{
     
