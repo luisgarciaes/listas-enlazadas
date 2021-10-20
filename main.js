a1c5c8b69259880d8291d6e1d833228277a0bf6c
ruruisu <zaddozgl@gmail.com>
2021-10-19 22:15:07 -0500

    corrected button behaviour

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ class Product{
         this.next=null;
     }
     info(){
-        return this._id + ":-> " + this._name;
+        return this._id + "-> " + this._name + " " + this._units + " " + this._cost;
       }
     
 }
@@ -34,7 +34,7 @@ class Inventory{
         }
         return x;
     }
-    search(id){
+    find(id){
         if (!this.start)
           return null;
         let aux=this.start;
@@ -45,7 +45,7 @@ class Inventory{
         }
         return null;
       }
-      delete(id){
+    delete(id){
         let deleted=null;
         if (!this.start)
           return null;
@@ -74,10 +74,96 @@ class Inventory{
 
 }
 class App{
+    constructor(){
+    this._inventory = new Inventory()
+
+    //this._consoleText = document.querySelector("#console")
+    let btnAdd = document.getElementById("btnAdd");
+    btnAdd.addEventListener("click", this._addProduct);
+    let btnRemove = document.querySelector("#btnRemove");
+    btnRemove.addEventListener("click", this._showArr)
+    let btnFind = document.querySelector("#btnFind");
+    btnFind.addEventListener("click", this._showArr);
+    let btnReturn = document.querySelector("#btnReturn");
+    btnReturn.addEventListener("click", this._returnInventory);
+    let btnReturnReverse = document.querySelector("#btnReturnReverse");
+    btnReturnReverse.addEventListener("click", this._showArr);
+    }
+    _updateConsole = (x,y) =>{
+        let message = "";
+        switch (y) {
+            case 1:
+                message = "Se agregó el producto " + x._name + " correctamente.";
+                break;
+            case 2:
+                message = "Se borró el producto " + x._name + " correctamente.";
+                break;
+            case 3:
+                message = "Se borró el producto " + x._name + " correctamente.";
+                break;
+            case 4:
+                message = "Se borró el producto " + x._name + " correctamente.";
+                break;
+            case 5:
+                message = this._inventory.toString();
+                break;
+        }
+    }
+    _returnInventory(){
+        if(this._inventory === undefined){
+            console.log("good")
+            return "good";
+        }
+        let x = this._inventory.toString();
+        return x;
+    }
+    _checkIfFull(){
+        let regSize = this._inventory.length;
+        if(regSize >= 19){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    _addProduct = () => {
+        if(this._checkIfFull()){
+            alert("full");
+            return;
+        }
+
+       let product = new Product(document.getElementById("id").value,document.getElementById("name").value
+        ,document.getElementById("units").value,document.getElementById("cost").value);
+
+        this._inventory.add(product);
+        this._updateConsole(product,1)
+        console.log(this._inventory.list())
+
+    }
     
+    _addProductInPosition = (x) => {
+        y = this._inventory.find(x)
+        if (y == null) {
+            console.log("A product named '" + x.info() + "' does not exist.")
+        }else{
+            console.log("The product '" + x.info() + "' has been replaced.")
+        }
+
+    };
+
+    _removeProduct = (x) => {
+        y = this._inventory.eliminate(x)
+        if (y == null) {
+            console.log("The product with the ID '" + x + "' does not exist.")
+        }else{
+            console.log("The product '" + x.info() + "' has been deleted.")
+        }
+
+    };
+
+
 }
 
-let i=new Inventory();
+/*let i=new Inventory();
 let p1=new Product(1,1,1,1,1);
 i.add(p1);
 p1=new Product(2,1,1,1,1);
@@ -91,4 +177,7 @@ i.add(p1);
 p1=new Product(7,1,1,1,1);
 i.add(p1);
 console.log(i.list());
-console.log(i.search(4));
+console.log(i.find(4));*/
+
+
+new App;
