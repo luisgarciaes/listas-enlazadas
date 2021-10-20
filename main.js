9bed9b5e2e35a4beadd357839d8bececba23df6c
ruruisu <zaddozgl@gmail.com>
2021-10-19 19:54:16 -0500

    list function

--- main.js
+++ main.js
@@ -7,14 +7,35 @@ class Product{
         this._totalCost = this._units * this._cost;
         this.next=null;
     }
+    info(){
+        return this._id + ":-> " + this._name;
+      }
+  }
 }
 class Inventory{
     constructor(){
         this.start=null;
     }
     add(){
-
+        if(this.start==null){this.start=x;}
+        else{this.add(x,this.start)}
     }
+    _add(){
+        if(last.next==null){last.next=nuevo}
+        else{this._agregar(nuevo,last.next)}
+    }
+
+    list(){
+        let x = '';
+        let temp=this.start;
+        while (temp!=null){
+          x += temp.info() + "\n"
+          temp=temp.next;
+        }
+        return x;
+    }
+
+
 }
 class App{
     
